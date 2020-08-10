97ec9537e7981dae0130bc4d6a78d09467cffa4b
SamanwoySaha <samanwoy.kuet@gmail.com>
2020-08-10 19:33:34 +0600

    populate ui with saved data and final commit

--- script.js
+++ script.js
@@ -26,6 +26,27 @@ function updateSelectedCount() {
     total.innerHTML = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+populateUI();
+
 // movie click event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
@@ -41,4 +62,8 @@ container.addEventListener('click', e => {
         updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
+
 
